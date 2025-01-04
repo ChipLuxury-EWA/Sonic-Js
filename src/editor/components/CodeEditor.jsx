1e14b632a76036eb214d104d9a5dc360813ff5b1
ChipLuxury-EWA <chip.luxury.ewa@gmail.com>
2025-01-04 20:52:27 +0200

    working infra

--- src/editor/components/CodeEditor.jsx
+++ src/editor/components/CodeEditor.jsx
@@ -1,11 +1,34 @@
 import { useRef } from "react";
 import Editor from "@monaco-editor/react";
+import * as monaco from "monaco-editor";
 // eslint-disable-next-line
 import * as Tone from "tone";
 import ButtonsRow from "./ButtonsRow";
 
 const CodeEditor = () => {
   const editorRef = useRef(null);
+
+  const handleEditorWillMount = (monaco) => {
+    monaco.languages.registerCompletionItemProvider("javascript", {
+      provideCompletionItems: () => {
+        const suggestions = [
+          {
+            label: "fooooooo",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "console.log()",
+            detail: "Log output to console",
+          },
+          {
+            label: "setTimeout",
+            kind: monaco.languages.CompletionItemKind.Function,
+            insertText: "setTimeout(() => {\n  \n}, 1000);",
+            detail: "Execute a function after a delay",
+          },
+        ];
+        return { suggestions: suggestions };
+      },
+    });
+  };
 
   const handleEditorDidMount = (editor) => {
     editorRef.current = editor;
@@ -33,6 +56,7 @@ const CodeEditor = () => {
         defaultLanguage="javascript"
         defaultValue={defaultToneScript}
         theme="vs-dark"
+        beforeMount={handleEditorWillMount}
         onMount={handleEditorDidMount}
       />
     </div>
